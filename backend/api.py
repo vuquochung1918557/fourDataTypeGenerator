f51ac70636edd2f62f83c5a7158dca936aa5c6c1
Paul <vuquochung1918@outlook.com>
2021-02-22 13:53:38 +0700

    Dockerized

--- backend/api.py
+++ backend/api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(FourTypeOfData, '/theAPI/')
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0',debug=True)
 
 
         
